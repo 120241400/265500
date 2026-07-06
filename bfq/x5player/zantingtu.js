ce0170172d228662c6ed98d6403e11a6906d9353
120241400 <89237995+120241400@users.noreply.github.com>
2026-07-06 09:07:36 +0800

    Update zantingtu.js

--- bfq/x5player/zantingtu.js
+++ bfq/x5player/zantingtu.js
@@ -1,3 +1,3 @@
 // 暂停遮挡图配置
 // 修改下面的链接即可更换暂停时显示的图片
-window.PAUSE_IMAGE_URL = 'https://file.uhsea.com/2607/d2ce917a756959809d5bfdd6955ada06AF.png?https://2658500.vip/bfq/x5player/biyanzhomian.png';
+window.PAUSE_IMAGE_URL = 'https://i0.wp.com/2658500.vip/bfq/x5player/haibao2.png';
