592cf158106023ace1ddab95cbef9be653493f17
120241400 <89237995+120241400@users.noreply.github.com>
2026-07-07 18:55:16 +0800

    Add files via upload

---
+++ bfq/x5player/player-config.js
@@ -0,0 +1,25 @@
+﻿
+// ========== 暂停遮挡图配置 ==========
+// 暂停时显示的图片链接
+window.PAUSE_IMAGE_URL = 'https://aisearch.cdn.bcebos.com/fileManager/KxNSA_0f9QdRc1QJKNiqOg/1781180459409EixVFl.jpg';
+
+// 暂停图片停留时间（秒），设为 0 表示不自动消失
+window.PAUSE_OVERLAY_SECONDS = 3;
+
+
+
+
+// ========== 首次播放倒计时弹窗配置 ==========
+window.AD_CONFIG = {
+  // 是否开启倒计时弹窗功能：yes 开启 / no 关闭
+  enabled: 'yes',
+
+  // 倒计时弹窗图片链接
+  imageUrl: 'https://bing.img.run/rand.php',
+
+  // 点击跳转的网址
+  linkUrl: 'https://www.baidu.com',
+
+  // 倒计时秒数
+  seconds: 5
+};
